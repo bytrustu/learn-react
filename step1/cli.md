e23e95b8dbcad6d16036a2ea5f6bc49452421181
bytrustu <bytrustu@gmail.com>
2020-07-23 08:06:51 +0900

    css&scss

--- step1/cli.md
+++ step1/cli.md
@@ -2,4 +2,10 @@ npx create-react-app cra-test
 HTTPS=true npm start  
 npm run build  
 CI=true npm test  
-npm install core-js
+npm install core-js  
+
+
+npx create-react-app css-test  
+npm install classname  
+npm install node-sass  
+npm install styled-components
---
+++ step1/css-test/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import Button from "./Box4";
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <Button size="big"/>
+        <Button size="small"/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
