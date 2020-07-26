99aeef2a34d9784dc0d2460270ee6fe7914b49a2
bytrustu <bytrustu@gmail.com>
2020-07-26 23:42:15 +0900

    component recipe step1

---
+++ step4/recipe/src/ComponentRecipe.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+MyComponent.propTypes = {
+
+}
+
+export default function MyComponent({prop1, prop2}) {
+
+}
+
+const COLUMES = [
+    {id: 1, name: 'phoneNumber', width: 200, color: 'white'},
+    {id: 1, name: 'city', width: 100, color: 'grey'},
+]
+
+const URL_PRODUCT_LIST = '/api/products';
+function getTotalPrice({ price, total }) {
+    
+}
