60fdec4880e47d9abe600f3d3d221f2d278a95ff
bytrustu <bytrustu@gmail.com>
2020-07-30 01:08:47 +0900

    friend list view

--- step6/redux-test/src/index.js
+++ step6/redux-test/src/index.js
@@ -1,41 +1,12 @@
-import { createStore, combineReducers } from "redux";
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-import timelineReducer, {
-    addTimeline,
-    removeTimeline,
-    editTimeline,
-    increaseNextPage
-} from './timeline/state';
-
-import friendReducer, {
-    addFriend,
-    removeFriend,
-    editFriend
-} from './friend/state';
 import TimelineMain from "./timeline/container/TimelineMain";
+import FriendMain from "./friend/container/FriendMain";
 
-const reducer = combineReducers({
-    timeline: timelineReducer,
-    friend: friendReducer
-});
-
-const store = createStore(reducer);
-store.subscribe(() => {
-    const state = store.getState();
-    console.log(state);
-});
-
-store.dispatch(addTimeline({ id: 1, desc: '코딩은 즐거워' }));
-store.dispatch(addTimeline({ id: 2, desc: '리덕스 조와' }));
-store.dispatch(increaseNextPage());
-store.dispatch(editTimeline({ id: 2, desc: '리덕스 너무 조와' }));
-store.dispatch(removeTimeline({ id: 1, desc: '코딩은 즐거워' }))
-
-store.dispatch(addFriend({ id: 1, name: '아이유' }));
-store.dispatch(addFriend({ id: 2, name: '수지' }));
-store.dispatch(editFriend({ id: 2, name: '조보아' }));
-store.dispatch(removeFriend({ id: 1, name: '아이유' }));
-
-ReactDOM.render(<div><TimelineMain/></div>, document.querySelector('#root'));
+ReactDOM.render(
+    <div>
+        <TimelineMain/>
+        <FriendMain/>
+    </div>,
+    document.querySelector('#root')
+);
